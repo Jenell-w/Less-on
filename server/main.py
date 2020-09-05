3a6bfbbb5a754fad1ec5af3b7e426697c1c31e38
Jenell-w <jenellweitz@gmail.com>
2020-09-04 22:42:50 -0600

    attempting to create db

--- server/main.py
+++ server/main.py
@@ -1,21 +1,21 @@
 from app import create_app, setup_database
 from flask import render_template
 
+
 def add_vue_routes(app):
     @app.route('/')
     def serve_vue_app():
         """
-        Server our vue app
+        Serve our vue app
         """
         return(render_template('index.html'))
-
 
     @app.after_request
     def add_header(req):
         """
         Clear Cache for hot-reloading
         """
-        req.headers["Cache-Control"] = "no-cache"
+        req.headers['Cache-Control'] = "no-cache"
         return req
 
 

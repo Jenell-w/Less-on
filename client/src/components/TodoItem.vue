53ac8059155b6e1fa8dc933e27155e4aef912f8c
Jenell-w <jenellweitz@gmail.com>
2020-09-23 16:52:46 -0600

    updating packages

--- client/src/components/TodoItem.vue
+++ client/src/components/TodoItem.vue
@@ -1,35 +1,23 @@
 <template>
-    <div class="todo-item-container">
-      <li @click="toggleTodo" v-bind:class="{ 'done-todo': todo.done }">{{todo.item}}</li>
-      <button @click="deleteTodo">X</button>
-    </div>
+  <div class="todo-item-container">
+    <li @click="toggleTodo" v-bind:class="{ 'done-todo': todo.done }">{{todo.item}}</li>
+    <button @click="deleteTodo">X</button>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-  name: 'TodoItem',
-  props: ['todo', 'getAllTodos'],
+  name: "Assignment",
+  props: ["todo", "getAllTodos"],
 
   methods: {
     toggleTodo() {
-      axios.patch('/todo', {id: this.todo.id})
-        .then(() => this.getAllTodos())
+      axios.patch("/todo", { id: this.todo.id }).then(() => this.getAllTodos());
     },
-    deleteTodo() {
-      axios.delete('/todo/' + this.todo.id)
-        .then(() => this.getAllTodos())
-    }
-  }
-}
+  },
+};
 </script>
 
 <style>
-.done-todo {
-  text-decoration: line-through;
-}
-
-.todo-item-container {
-  display: flex;
-}
 </style>
